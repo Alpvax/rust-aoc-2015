3f01c048e6bdf5b2f93c216a167773a981021324
Alpvax <development@alpvax.uk>
2020-10-23 16:58:00 +0100

    Add day19.2. Requires optimising

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -9,7 +9,8 @@ fn main() {
     let mut map = HashMap::new();
     let input = load(&mut map);
     //println!("Input: {}\n{:?}", input, map);
-    println!("Part 1: {:?}", part1(&input, &map));
+    println!("Part 1: {}", part1(&input, &map));
+    println!("Part 2: {}", part2(&input, &map));
 }
 
 fn load(map: &mut HashMap<String, Vec<String>>) -> String {
@@ -45,7 +46,51 @@ fn part1(input: &str, map: &HashMap<String, Vec<String>>) -> usize {
 }
 
 fn part2(target: &str, map: &HashMap<String, Vec<String>>) -> u32 {
-    let target_len = target.chars().size();
-    let mut min_steps = u32::MAX;
+    let target_len = target.len(); // ASCII, so len consistent
+    let mut processing = HashSet::new();
+    processing.insert(String::from("e"));
+    let mut processed = HashSet::new();
+    let mut count = 0;
+    loop {
+        count += 1;
+        //println!("Iteration {}: Processing: {}\n{:?}", count, processing.len(), processing);
+        let mut next = HashSet::new();
+        for s in processing.iter().cloned() {
+            processed.insert(s.to_owned());
+            match replace1(&s, &map, target, target_len) {
+                ReplResult::Found => return count,
+                ReplResult::Produced(set) => {
+                    for res in set {
+                        next.insert(res);
+                    }
+                }
+            }
+        }
+        processing.clear();
+        for res in next.difference(&processed) {
+            processing.insert(res.to_string());
+        }
+    }
+}
 
+fn replace1(input: &str, map: &HashMap<String, Vec<String>>, target: &str, target_len: usize) -> ReplResult {
+    let mut results = HashSet::new();
+    for (k, v) in map.iter() {
+        for m in Regex::new(&k).unwrap().find_iter(input) {
+            for r in v {
+                let s = format!("{}{}{}", &input[..m.start()], r, &input[m.end()..]);
+                if s == target {
+                    return ReplResult::Found;
+                } else if s.len() < target_len {
+                    results.insert(s);
+                }
+            }
+        }
+    }
+    ReplResult::Produced(results)
+}
+
+enum ReplResult {
+    Found,
+    Produced(HashSet<String>),
 }
